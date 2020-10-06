cbaac345e7aa6806d00ac2d30cd3e3ce182e8fea
Usairim Isani <usairim.isani@outlook.com>
2020-10-06 14:36:59 +0500

    wacell

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,16 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+use js_sys::Math;
 use std::fmt;
+extern crate web_sys;
+
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -55,6 +64,13 @@ impl Universe {
                 let idx = self.get_index(row, col);
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
+                log!(
+                    "cell[{}, {}] is initially {:?} and has {} live neighbors",
+                    row,
+                    col,
+                    cell,
+                    live_neighbors
+                );
                 let next_cell = match (cell, live_neighbors) {
                     (Cell::Alive, x) if x < 2 => Cell::Dead,
                     (Cell::Alive, x) if x > 3 => Cell::Dead,
@@ -68,11 +84,12 @@ impl Universe {
         self.cells = next;
     }
     pub fn new() -> Universe {
+        utils::set_panic_hook();
         let width = 200;
         let height = 64;
         let cells = (0..width * height)
             .map(|i| {
-                if (i % 2 == 0 || i % 13 == 0) && i % 31 > 1 {
+                if i % 2 == 0 || i % 7 == 0 || Math::random() > 0.5 {
                     Cell::Alive
                 } else {
                     Cell::Dead
@@ -84,6 +101,18 @@ impl Universe {
             height,
             cells,
         }
+    }
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
     }
     pub fn render(&self) -> String {
         self.to_string()
@@ -112,5 +141,20 @@ impl fmt::Display for Universe {
         }
 
         Ok(())
+    }
+}
+impl Universe {
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
     }
 }
